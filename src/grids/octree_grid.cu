47982428db0fac92cce385fdd2e079dcda05245e
hlaw <hlaw@atinea.pl>
2023-08-29 23:41:48 +0200

    wyznaczanie id za pomocą kodu Mortona

--- src/grids/octree_grid.cu
+++ src/grids/octree_grid.cu
@@ -9,6 +9,100 @@
 #define MANTIS_MASK 0x007fffff
 #define MANTIS_OR_MASK 0x00800000
 
+
+#define max(a,b) ( a > b ? a : b)
+#define min(a,b) ( a > b ? b : a)
+
+__device__ unsigned int partEveryByteByTwo(unsigned int n)
+{
+	n = (n ∧ (n << 16)) & 0xff0000ff;
+	n = (n ∧ (n << 8))  & 0x0300f00f;
+	n = (n ∧ (n << 4))  & 0x030c30c3;
+	n = (n ∧ (n << 2))  & 0x09249249;
+	return n;
+}
+
+	__device__ unsigned int calculateMortonCodeIdForCell(unsigned int xId, unsigned int yId, unsigned int zId)
+	{
+		return (partEveryByteByTwo(z) << 2) | (partEveryByteByTwo(y) << 1) | partEveryByteByTwo(x);
+	}
+
+	__global__ void calculateCellIdKernel(const float* positionX, const float* positionY, const float* positionZ,
+		unsigned int* cellIds, unsigned int* particleIds, const unsigned int particleCount,
+		unsigned int cellWidth, unsigned int cellHeight, unsigned int cellDepth)
+	{
+		unsigned int particleId = blockIdx.x * blockDim.x + threadIdx.x;
+		if (particleId >= particleCount)
+			return;
+
+		unsigned int cellId = calculateMortonCodeIdForCell(unsigned int(positionX[particleId]/cellWidth), 
+			unsigned int(positionY[particleId]/cellHeight), unsigned int(positionZ[particleId]/cellDepth));
+
+		particleIds[particleId] = particleId;
+		cellIds[particleId] = cellId;
+	}
+
+	__global__ void calculateStartAndEndOfCellKernel(const float* positionX, const float* positionY, const float* positionZ,
+		const unsigned int* cellIds, const unsigned int* particleIds,
+		unsigned int* cellStarts, unsigned int* cellEnds, unsigned int particleCount)
+	{
+		unsigned int id = blockIdx.x * blockDim.x + threadIdx.x;
+		if (id >= particleCount)
+			return;
+
+		unsigned int currentCellId = cellIds[id];
+
+		// Check if the previous cell id was different - it would mean we found the start of a cell
+		if (id > 0 && currentCellId != cellIds[id - 1])
+		{
+			cellStarts[currentCellId] = id;
+		}
+
+		// Check if the next cell id was different - it would mean we found the end of a cell
+		if (id < particleCount - 1 && currentCellId != cellIds[id + 1])
+		{
+			cellEnds[currentCellId] = id;
+		}
+
+		if (id == 0)
+		{
+			cellStarts[cellIds[0]] = 0;
+		}
+		if (id == particleCount - 1)
+		{
+			cellStarts[cellIds[particleCount - 1]] = particleCount - 1;
+		}
+	}
+
+	void createOctreeGridData(const float* positionX, const float* positionY, const float* positionZ, unsigned int* gridCellIds, 
+		unsigned int* particleIds, float cellWidth, float cellHeight, float cellDepth, unsigned int* gridCelLStarts, 
+		unsigned int* gridCellEnds, unsigned int objectCount)
+	{
+		// Calculate launch parameters
+
+		const int threadsPerBlock = objectCount > 1024 ? 1024 : objectCount;
+		const int blocks = (objectCount + threadsPerBlock - 1) / threadsPerBlock;
+
+		// 1. Calculate cell id for every particle and store as pair (cell id, particle id) in two buffers
+		gridHelpers::calculateCellIdKernel << <blocks, threadsPerBlock >> >
+			(positionX, positionY, positionZ, gridCellIds, particleIds, objectCount, cellWidth, cellHeight, cellDepth);
+
+		// 2. Sort particle ids by cell id
+
+		thrust::device_ptr<unsigned int> keys = thrust::device_pointer_cast<unsigned int>(gridCellIds);
+		thrust::device_ptr<unsigned int> values = thrust::device_pointer_cast<unsigned int>(particleIds);
+
+		thrust::stable_sort_by_key(keys, keys + objectCount, values);
+
+		// 3. Find the start and end of every cell
+
+		gridHelpers::calculateStartAndEndOfCellKernel << <blocks, threadsPerBlock >> >
+			(positionX, positionY, positionZ, gridCellIds, particleIds, gridCellStarts, gridCellEnds, objectCount);
+	}
+
+
+
+
 __global__ void calculateTreeLeafsCells(const unsigned int* cellIds, const unsigned int objectCount,
 	const unsigned int cellCountX, const unsigned int cellCountY, const unsigned int cellCountZ,
 	const unsigned int cellWidth, const unsigned int cellHeight, const unsigned int cellDepth, const unsigned int divisionCount
@@ -18,7 +112,9 @@ __global__ void calculateTreeLeafsCells(const unsigned int* cellIds, const unsig
 	if (objectId >= objectCount)
 		return;
 
-	unsigned int cellId = cellIds[objectId];
+	unsigned int cellMortonCodeId = cellIds[objectId];
+	unsigned int currentCellAbsoluteId = cellId + (pow(8, levels) - 1)/7;
+	unsigned int parentCellAbsoluteId = (cellId - 1)/8;
 
 	const unsigned int idX = (cellId % cellWidth) / 2;
 	const unsigned int idY = ((cellId / cellWidth) % cellHeight) / 2;
@@ -54,8 +150,7 @@ __global__ void calculateTreeLeafsCells(const unsigned int* cellIds, const unsig
 
 #pragma unroll
 	while (counter++ < divisionCount) {
-		
-
+		unsigned char shift = 0x80 >> ;
 		__syncthreads();
 	}
 
@@ -112,7 +207,7 @@ OctreeGrid::OctreeGrid(const unsigned int objectCount, const unsigned int levels
 
 void OctreeGrid::calculateGrid(const float* positionX, const float* positionY, const float* positionZ, unsigned int particleCount)
 {
-	gridHelpers::createUniformGrid(positionX, positionY, positionZ, gridCellIds, particleIds, cellWidth,
+	createOctreeGridData(positionX, positionY, positionZ, gridCellIds, particleIds, cellWidth,
 		cellHeight, cellDepth, gridCellStarts, gridCellEnds, particleCount);
 
 
