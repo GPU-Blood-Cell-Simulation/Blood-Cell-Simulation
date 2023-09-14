a76c1ced4047fe6443bf5857102bccba8f2fda23
feedop <110346745+feedop@users.noreply.github.com>
2023-09-14 21:40:06 +0200

    Final touch

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Blood Cell Simulation
+
+Blood Cell Simulation created in CUDA and OpenGL by:
+* Hubert Lawenda
+* Piotr Nieciecki
+* Filip Skrzeczkowski
+
+## How to build?
+
+Use the Visual Studio 2022 .sln file to build the project. CUDA 11.8 is required.
+
+## How to run?
+
+The program takes no input. Every parameter is defined in defines.hpp as a compile-time constant.
+
+## How to move the camera?
+
+* WASD - move around
+* Space - ascedn
+* LShift - descend
+* Arrow keys - rotate the camera
+
+* V - switch between edges-only and filled display mode
--- src/defines.hpp
+++ src/defines.hpp
@@ -27,8 +27,8 @@ inline constexpr int cellCountY = static_cast<int>(height / cellHeight);
 inline constexpr int cellCountZ = static_cast<int>(depth / cellDepth);
 
 // blood cell parameters
-inline constexpr unsigned int particlesInBloodCell = 2;
-inline constexpr unsigned int bloodCellsCount = 500;
+inline constexpr unsigned int particlesInBloodCell = 4;
+inline constexpr unsigned int bloodCellsCount = 250;
 inline constexpr unsigned int particleCount = bloodCellsCount * particlesInBloodCell;
 
 inline constexpr float springsInCellsLength = 10;
@@ -71,10 +71,13 @@ inline constexpr float collisionSpringCoeff = 0.2f;
 inline constexpr float collisionDampingCoeff = 0.02f;
 inline constexpr float collistionShearCoeff = 0.05f;
 
-// Initaite velocity of particles
+// Initial velocity of particles
 inline constexpr float initVelocityX = 0.0f;
 inline constexpr float initVelocityY = -10.0f;
 inline constexpr float initVelocityZ = 0.0f;
+
+// Blood flow
+inline constexpr bool useBloodFlow = true;
 
 // Lighting
 inline constexpr bool useLighting = true;
